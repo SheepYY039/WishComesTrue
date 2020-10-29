f35088c8ad9f4b326bc15f9126d0b0eb114eb7d0
Kanav129 <73458114+Kanav129@users.noreply.github.com>
2020-10-29 23:14:20 +0800

    Update processwishes.php

--- processwishes.php
+++ processwishes.php
@@ -67,5 +67,39 @@
 		<tr><?php echo $_GET["info"];?></tr></td>
 	</tabel>
 
+    <button onclick="addstuff()" value="Confirm Wish">
+  <script>
+  function addstuff(){
+    document.innerHTML = "
+    <?php
+    $servername = "localhost";
+    $username = "id15251966_requested_wishes";
+    $password = "WCThk2020-WCThk2020";
+    $dbname = "id15251966_wishes";
+    $table = "tbl_wishes";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    //here we need to add to db
+    $sql = "INSERT INTO $table
+    	VALUES ($_GET['wish'], $_GET['name'],$_GET['email'],$_GET['phone'],$groups,$projects,$_GET['people'],$_GET['money'],$_GET['District'],$inputdate,$_GET['starttime'],$_GET['start'],$_GET['end'],$_GET['info'])";
+      if (mysqli_query($conn, $sql)) {
+          echo "New records created successfully<br>";
+      } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+
+    ?>
+    ";
+  }
+  </script>
+
 	</body>
 </html>
