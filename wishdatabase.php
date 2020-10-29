83666637f9a84ce74c714d98869be917b01d74ff
Kanav129 <73458114+Kanav129@users.noreply.github.com>
2020-10-29 23:10:19 +0800

    Update wishdatabase.php

--- wishdatabase.php
+++ wishdatabase.php
@@ -5,55 +5,15 @@
 <title>Database Creation</title>
 </head>
 <button onclick="addstuff()">
-<button onclick="createtable()">
+
 <script>
-function createtable(){
-  document.innerHTML = "
-  <?php
-  $servername = "localhost";
-  $username = "id15251966_requested_wishes";
-  $password = "/+S4S%1K^MId\dGb";
-  $dbname = "id15251966_wishes";
-  $table = "tbl_wishes";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
-
-  //here we need to add to db
-  // Create database
-
-
-  $sql = "CREATE TABLE $table" (
-    Name varchar(50) UNSIGNED PRIMARY KEY,
-    Phone int(8),
-    Email varchar (50),
-    Minority groups varchar(25),
-    Project type varchar(25),
-    Start date varchar(25),
-    End date varchar(25),
-    Additional information varchar(50),
-    )
-    if ($conn->query($sql) === TRUE) {
-  echo "Table tbl_wishes created successfully";
-    else {
-  echo "Error creating table: " . $conn->error;
-  mysqli_close($conn);
-  ?>
-  ";
-}
 
 function addstuff(){
   document.innerHTML = "
   <?php
   $servername = "localhost";
   $username = "id15251966_requested_wishes";
-  $password = "/+S4S%1K^MId\dGb";
+  $password = "WCThk2020-WCThk2020";
   $dbname = "id15251966_wishes";
   $table = "tbl_wishes";
 
@@ -67,8 +27,13 @@ function addstuff(){
   echo "Connected successfully";
 
   //here we need to add to db
-
-
+  $sql = "INSERT INTO $table
+  	VALUES ($_GET['wish'], $_GET['name'],$_GET['email'],$_GET['phone'],$groups,$projects,$_GET['people'],$_GET['money'],$_GET['District'],$inputdate,$_GET['starttime'],$_GET['start'],$_GET['end'],$_GET['info'])";
+    if (mysqli_query($conn, $sql)) {
+        echo "New records created successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
   ?>
   ";
